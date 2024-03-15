f15f22e70f52fb3dee6ca87cca7247506d54de10
LbnMahs <labanmahs2002@gmail.com>
2024-03-15 14:10:12 +0300

    update(main.dart): setting up the theme and bloc providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,66 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:traxi/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() async {
+import 'package:traxi/data/auth/user_data_provider.dart';
+import 'package:traxi/data/auth/user_repository.dart';
+import 'package:traxi/firebase_options.dart';
+import 'package:traxi/middleware/auth/bloc/user_auth_bloc.dart';
+import 'package:traxi/views/screens/splash_screen.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<UserRepository>(
+          create: (context) => UserRepository(UserAuthDataProvider()),
+        ),
+      ], 
+      child: const MyApp()
+    )
+  );
 }
+
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.dark,
+  ),
+  useMaterial3: true,
+  textTheme: GoogleFonts.latoTextTheme(),
+  appBarTheme: AppBarTheme(
+    titleTextStyle: GoogleFonts.poppins(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: Colors.blue,
+    unselectedItemColor: Colors.grey.shade400,
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Your app name',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Your Title here'),
-        ),
-        body: const Center(
-          child: Text('Hello World, of course'),
-        ),
+      title: 'Traxi',
+      theme: theme,
+      home: MultiBlocListener(
+        listeners: [
+          BlocListener<UserAuthBloc, UserAuthState>(
+            listener: (context, state) {
+
+            }
+          )
+        ],
+        child: const SplashScreen(),
       ),
     );
   }
