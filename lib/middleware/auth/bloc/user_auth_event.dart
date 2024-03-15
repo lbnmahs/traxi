e3cee4fd831678ecd160eb3a0522ce2155d75771
LbnMahs <labanmahs2002@gmail.com>
2024-03-15 12:55:54 +0300

    update: done with user auth events

--- lib/middleware/auth/bloc/user_auth_event.dart
+++ lib/middleware/auth/bloc/user_auth_event.dart
@@ -2,3 +2,21 @@ part of 'user_auth_bloc.dart';
 
 @immutable
 sealed class UserAuthEvent {}
+
+class SignInEvent extends UserAuthEvent {
+  final String email;
+  final String password;
+
+  SignInEvent(this.email, this.password);
+}
+
+class SignUpEvent extends UserAuthEvent {
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String password;
+
+  SignUpEvent(this.email, this.firstName, this.lastName, this.password);
+}
+
+class SignOutEvent extends UserAuthEvent {}
